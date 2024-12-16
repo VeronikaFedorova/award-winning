70cf2a001e1163d3c57c0d7bc047bc360f91eb8d
Veronika Fedorova <fedorovavv03@gmail.com>
2024-12-16 14:34:49 +0200

    Added Animated Title section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from 'gsap/all';
+import AnimatedTitle from './AnimatedTitle';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -30,12 +31,10 @@ const About = () => {
         <h2 className='font-zentry text-sm uppercase md:text-[10px]'>
           Welcome to Zentry
         </h2>
-        <div className='mt-5 font-extrabold text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
-          Discover the world's <br /> largest shared adventure
-          <div className='about-subtext'>
-            <p>The Game of Games begins-your life, now an epic MMORPG</p>
-            <p>Zentry unites every player from countless games and platforms</p>
-          </div>
+        <AnimatedTitle title="Discover the world's largest shared adventure" containerClass="mt-5 !text-black text-center" />
+        <div className='about-subtext'>
+          <p>The Game of Games begins-your life, now an epic MMORPG</p>
+          <p>Zentry unites every player from countless games and platforms</p>
         </div>
         <div className='h-dvh w-screen' id='clip'>
           <div className='mask-clip-path about-image'>
