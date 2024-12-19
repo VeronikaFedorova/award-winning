3ef04bac8899d2ebc737c25b583247c3d6195121
Veronika Fedorova <fedorovavv03@gmail.com>
2024-12-19 15:46:05 +0200

    Added a Footer component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
-import AnimatedTitle from "./AnimatedTitle";
-import Button from "./Button";
+import AnimatedTitle from './AnimatedTitle';
+import Button from './Button';
 
 const ImageClipBox = ({ src, clipClass }) => (
   <div className={clipClass}>
@@ -9,7 +9,7 @@ const ImageClipBox = ({ src, clipClass }) => (
 
 const Contact = () => {
   return (
-    <div id='contact' className='my-20 min-h-96 w-screen px-10'>
+    <div id='contact' className='my-20 min-h-96 w-screen  px-10'>
       <div className='relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden'>
         <div className='absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96'>
           <ImageClipBox
@@ -32,6 +32,7 @@ const Contact = () => {
             clipClass='sword-man-clip-path md:scale-125'
           />
         </div>
+
         <div className='flex flex-col items-center text-center'>
           <p className='mb-10 font-general text-[10px] uppercase'>
             Join Zentry
